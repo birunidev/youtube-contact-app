6df80f1bc3d9445787384ea1d237a4b01a5acae8
birunidev <birunimamak@gmail.com>
2021-02-15 10:17:02 +0700

    final code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
 import "./App.css";
 import List from "./List";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "John Doe",
+      telp: "08813452521",
+    },
+    {
+      id: 2,
+      name: "Alex Picker",
+      telp: "08815646413",
+    },
+  ]);
+  const [isUpdate, setIsUpdate] = useState({ id: null, status: false });
+
+  const [formData, setFormData] = useState({
+    name: "",
+    telp: "",
+  });
+
+  function handleChange(e) {
+    let newFormState = { ...formData };
+    newFormState[e.target.name] = e.target.value;
+    setFormData(newFormState);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let data = [...contacts];
+    if (isUpdate.status) {
+      data.forEach((contact) => {
+        if (contact.id === isUpdate.id) {
+          contact.name = formData.name;
+          contact.telp = formData.telp;
+        }
+      });
+    } else {
+      data.push({
+        id: uid(),
+        name: formData.name,
+        telp: formData.telp,
+      });
+    }
+    setContacts(data);
+    setIsUpdate(false);
+    setFormData({ name: "", telp: "" });
+  }
+
+  function handleEdit(id) {
+    // cari data di state
+    // isi data ke state form
+    let data = [...contacts];
+    let foundData = data.find((contact) => contact.id === id);
+    setIsUpdate({ status: true, id: id });
+    setFormData({ name: foundData.name, telp: foundData.telp });
+  }
+
+  function handleDelete(id) {
+    let data = [...contacts];
+    let filteredData = data.filter((contact) => contact.id !== id);
+    setContacts(filteredData);
+  }
+
   return (
     <div className="App">
       <h1 className="px-3 py-3 font-weight-bold">My Contact List</h1>
-
-      <form className="px-3 py-4">
+      <form onSubmit={handleSubmit} className="px-3 py-4">
         <div className="form-group">
           <label htmlFor="">Name</label>
-          <input type="text" className="form-control" name="name" />
+          <input
+            type="text"
+            onChange={handleChange}
+            className="form-control"
+            value={formData.name}
+            name="name"
+          />
         </div>
         <div className="form-group mt-3">
           <label htmlFor="">No. Telp</label>
-          <input type="text" className="form-control" name="telp" />
+          <input
+            type="text"
+            onChange={handleChange}
+            value={formData.telp}
+            className="form-control"
+            name="telp"
+          />
         </div>
         <div>
           <button type="submit" className="btn btn-primary w-100 mt-3">
@@ -22,7 +97,11 @@ function App() {
         </div>
       </form>
 
-      <List />
+      <List
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        data={contacts}
+      />
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,28 +1,32 @@
 import React from "react";
 
-export default function List() {
+export default function List({ data, handleEdit, handleDelete }) {
   return (
     <div className="list-group">
-      <div className="list-group-item list-group-item-action">
-        <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">John Doe</h5>
-          <div>
-            <button className="btn btn-sm btn-link">Edit</button>
-            <button className="btn btn-sm btn-link">Del</button>
+      {data.map((contact, index) => {
+        return (
+          <div key={index} className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">{contact.name}</h5>
+              <div>
+                <button
+                  onClick={() => handleEdit(contact.id)}
+                  className="btn btn-sm btn-link"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(contact.id)}
+                  className="btn btn-sm btn-link"
+                >
+                  Del
+                </button>
+              </div>
+            </div>
+            <p className="mb-1">{contact.telp}</p>
           </div>
-        </div>
-        <p className="mb-1">08815646413</p>
-      </div>
-      <div className="list-group-item list-group-item-action">
-        <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">Alex Pick</h5>
-          <div>
-            <button className="btn btn-sm btn-link">Edit</button>
-            <button className="btn btn-sm btn-link">Del</button>
-          </div>
-        </div>
-        <p className="mb-1">08815646413</p>
-      </div>
+        );
+      })}
     </div>
   );
 }
